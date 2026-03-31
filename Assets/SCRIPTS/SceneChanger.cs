2d3cb5bcb3b305bc0cb44da3194b52249b46a200
Tauha Imran <161915837+tauhaimran@users.noreply.github.com>
2026-03-31 19:20:45 +0500

    prohect-finished!!

--- Assets/SCRIPTS/SceneChanger.cs
+++ Assets/SCRIPTS/SceneChanger.cs
@@ -9,4 +9,18 @@ public class SceneChanger : MonoBehaviour
         Debug.Log("Switching to scene: " + sceneName);
         SceneManager.LoadScene(sceneName);
     }
+
+    // Attach this to your Quit/Exit button's OnClick() event
+    public void QuitApp()
+    {
+        Debug.Log("Quit button pressed! Exiting the simulation...");
+        
+        // This closes the actual app on the Quest 2 headset
+        Application.Quit();
+        
+        // This magically stops the game if you are just testing inside the Unity Editor!
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
 }
